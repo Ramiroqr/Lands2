a1677215bdc02621cc20993b45867df6b8df2363
Ramiroqr <ramiroquisper@gmail.com>
2019-01-11 10:31:55 -0500

    Primer commit
    Este es el primer commit del proyecto

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,18 @@
+﻿namespace Lands.Infrastructure
+{
+    using Lands.ViewModels;
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            //patron de diseño locator
+            this.Main = new MainViewModel();        
+        }
+        #endregion
+    }
+}
